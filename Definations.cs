40af13eb90d1b953e590d4e2a1001dfb83ffe7a6
Zeeshan Khan <95034888+khanzeeshanf@users.noreply.github.com>
2023-07-01 22:58:21 +0530

    Create Definations.cs

---
+++ Definations.cs
@@ -0,0 +1,20 @@
+// A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+//A composite number is a positive integer that is not prime. In other words, it has a positive divisor other than one or itself
+
+// So if number is not prime then its composit
+
+/*
+      // Corner cases
+        if (n <= 1)
+            return "neither prime nor composite";
+  
+        for (int i = 2; i < n; i++){
+            if (n % i == 0)
+                return "composite";
+            else
+              return "Prime";
+      }
+*/
+
+
