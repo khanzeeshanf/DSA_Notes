3b126fe44d0f574fe0eb1ecb297c1e8f23814f52
Zeeshan Khan <95034888+khanzeeshanf@users.noreply.github.com>
2025-06-22 15:23:51 +0530

    Update Arrays.cs

--- Arrays.cs
+++ Arrays.cs
@@ -1,6 +1,7 @@
 //Arrays
 //
 
+//[1]
 //Array Reverse Best approche 
 //Using Two Pointers - O(n) Time and O(1) Space
 using System;
@@ -22,4 +23,31 @@ class ArrayOpration {
             right--; // Decrement the right pointer
         }
     }
+}
+
+//---------------------------------------------------------------//
+//[2]
+//Rotate an Array by d - Counterclockwise or Left
+//Using Reversal Algorithm - O(n) Time and O(1) Space
+
+void Reverse(int[] arr, int start, int end)
+{
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void LeftRotate(int[] arr, int d)
+{
+    int n = arr.Length;
+    d = d % n; // in case d > n
+
+    Reverse(arr, 0, d - 1);
+    Reverse(arr, d, n - 1);
+    Reverse(arr, 0, n - 1);
 }
